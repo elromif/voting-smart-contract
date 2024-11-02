ec00668de7f9f75c270be205375830b5dac9b94d
Romain Magne <romain.magne@berkeley.edu>
2024-11-02 23:40:34 +0100

    improvements phase 1

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -3,96 +3,120 @@
 pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract Voting is Ownable {
+/// @title Voting
+/// @notice This contract allows to setup an election, requiring voters to stake some eth into the contract to vote and allowing them to withdraw after the end of the election.
+/// @notice One can start and end the election using TIMER_ROLE, and setup a new election using SETUP_ROLE
+
+contract Voting is ReentrancyGuard, AccessControl {
     struct Candidate {
-        uint id;
+        uint256 id;
         string name;
-        uint votes;
+        uint256 votes;
         address candidate_address;
     }
 
-    event CastedVote(uint candidate_id);
+    event CastedVote(uint256 candidate_id);
+    event stakeWithdrawn(uint256 amount);
+    event votingPeriodStarted();
+    event votingPeriodEnded();
 
-    mapping(address => bool) address_has_voted_map;
+    mapping(address => uint) address_amount_deposited_map;
 
-    mapping(uint => Candidate) id_candidate_map;
+    mapping(uint256 => mapping(address => bool)) electionVoterMap;
 
-    uint candidatesCount;
+    Candidate[] candidates;
 
     bool isVotingPeriodRunning;
 
+    bytes32 public constant TIMER_ROLE = keccak256("TIMER_ROLE");
+    bytes32 public constant SETUP_ROLE = keccak256("SETUP_ROLE");
+
+    uint256 current_election_id;
+
     constructor(
         string[] memory _candidate_names,
-        address[] memory _candidate_addresses
-    ) Ownable(msg.sender) {
-        candidatesCount = _candidate_names.length;
+        address[] memory _candidate_addresses,
+        address setuper,
+        address timer
+    ) {
+        require(
+            _candidate_names.length == _candidate_addresses.length,
+            "The number of candidates and candidate addresses does not match"
+        );
         isVotingPeriodRunning = false;
-
-        for (uint i = 0; i < _candidate_names.length; i++)
-            id_candidate_map[i] = Candidate(
-                i,
-                _candidate_names[i],
-                0,
-                _candidate_addresses[i]
+        _grantRole(DEFAULT_ADMIN_ROLE, setuper);
+        _grantRole(SETUP_ROLE, setuper);
+        _grantRole(TIMER_ROLE, timer);
+        for (uint256 i = 0; i < _candidate_names.length; i++) {
+            candidates.push(
+                Candidate({
+                    id: i,
+                    name: _candidate_names[i],
+                    votes: 0,
+                    candidate_address: _candidate_addresses[i]
+                })
             );
+        }
+        current_election_id = 0;
     }
 
-    function getCandidate(uint id) public view returns (Candidate memory) {
-        return id_candidate_map[id];
+    function getCandidate(uint256 id) public view returns (Candidate memory) {
+        require(id >= 0 && id < candidates.length, "Wrong candidate id!");
+        return candidates[id];
     }
 
     function getCandidates() public view returns (Candidate[] memory) {
-        Candidate[] memory candidates = new Candidate[](candidatesCount);
-        for (uint i = 0; i < candidatesCount; i++) {
-            candidates[i] = id_candidate_map[i];
-        }
         return candidates;
     }
 
-    function getCandidatesCount() public view returns (uint) {
-        return candidatesCount;
+    function getcandidatesCount() public view returns (uint256) {
+        return candidates.length;
     }
 
-    function getBalance() public view returns (uint) {
+    function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
-    function vote(uint candidate_id) public payable {
+    function startVotingPeriod() public {
+        require(hasRole(TIMER_ROLE, msg.sender), "Caller is not a timer");
+        isVotingPeriodRunning = true;
+        emit votingPeriodStarted();
+    }
+
+    function vote(uint256 candidate_id) public payable nonReentrant {
         require(isVotingPeriodRunning == true, "Voting period not running!");
-        require(!address_has_voted_map[msg.sender], "You already voted!");
         require(
-            candidate_id < candidatesCount,
+            !electionVoterMap[current_election_id][msg.sender],
+            "You already voted!"
+        );
+        require(
+            candidate_id < candidates.length,
             "The candidate does not exist!"
         );
-        require(
-            msg.value > 0,
-            "You should contribute to the election's nest egg to vote !"
-        );
-        address_has_voted_map[msg.sender] = true;
-        id_candidate_map[candidate_id].votes += 1;
+        require(msg.value > 0, "You have to stake eth to vote !");
+        address_amount_deposited_map[msg.sender] = msg.value;
+        electionVoterMap[current_election_id][msg.sender] = true;
+        candidates[candidate_id].votes += 1;
         emit CastedVote(candidate_id);
-    }
-
-    function getCandidateVotesCount(
-        uint candidate_id
-    ) public view returns (uint votes_count) {
-        return id_candidate_map[candidate_id].votes;
-    }
-
-    function startVotingPeriod() public onlyOwner {
-        isVotingPeriodRunning = true;
-    }
-
-    function endVotingPeriod() public onlyOwner {
-        isVotingPeriodRunning = false;
-        address payable winner = payable(getWinner().candidate_address);
-        (bool sent, ) = winner.call{value: address(this).balance}("");
-        require(sent, "Failed to send the nest egg to the winner");
     }
 
     function getVotingPeriodStatus() public view returns (bool) {
         return isVotingPeriodRunning;
+    }
+
+    function getCandidateVotesCount(
+        uint256 candidate_id
+    ) public view returns (uint256 votes_count) {
+        return candidates[candidate_id].votes;
+    }
+
+    function endVotingPeriod() public {
+        require(hasRole(TIMER_ROLE, msg.sender), "Caller is not a timer");
+        isVotingPeriodRunning = false;
+        emit votingPeriodEnded();
     }
 
     function getWinner() public view returns (Candidate memory winner) {
@@ -100,19 +124,48 @@ contract Voting is Ownable {
             isVotingPeriodRunning == false,
             "The voting period is not finished yet!"
         );
-        uint winner_id = 0;
-        for (uint i = 0; i < candidatesCount; i++) {
-            if (id_candidate_map[i].votes > id_candidate_map[winner_id].votes) {
+        uint256 winner_id = 0;
+        for (uint256 i = 0; i < candidates.length; i++) {
+            if (candidates[i].votes > candidates[winner_id].votes) {
                 winner_id = i;
             }
         }
-        return id_candidate_map[winner_id];
+        return candidates[winner_id];
     }
 
-    function congratulateWinner() public payable {
-        require(msg.value > 0, "Amount must be greater than 0");
-        address payable winner = payable(getWinner().candidate_address);
-        (bool sent, ) = winner.call{value: msg.value}("");
-        require(sent, "Failed to send Ether to the winner");
+    function withdrawStake() public nonReentrant {
+        require(!isVotingPeriodRunning, "Voting period not finished yet!");
+        require(
+            address_amount_deposited_map[msg.sender] > 0,
+            "stake already withdrawn!"
+        );
+        uint256 amount = address_amount_deposited_map[msg.sender];
+        address_amount_deposited_map[msg.sender] = 0;
+        (bool sent, ) = msg.sender.call{value: amount}("");
+        require(sent, "Failed to withdraw stake");
+        emit stakeWithdrawn(amount);
+    }
+
+    function startNewElection(
+        string[] memory candidate_names,
+        address[] memory candidate_addresses
+    ) public {
+        require(hasRole(SETUP_ROLE, msg.sender), "Caller is not a setuper!");
+        require(
+            candidate_names.length == candidate_addresses.length,
+            "The number of candidates and candidate addresses does not match"
+        );
+        require(!isVotingPeriodRunning, "Current election not finished!");
+        delete candidates;
+        for (uint i = 0; i < candidate_names.length; i++) {
+            candidates.push(
+                Candidate({
+                    id: i,
+                    name: candidate_names[i],
+                    votes: 0,
+                    candidate_address: candidate_addresses[i]
+                })
+            );
+        }
     }
 }
--- test/Voting.ts
+++ test/Voting.ts
@@ -10,27 +10,21 @@ describe("Voting contract", function() {
 
     
     async function deploymentFixture(candidates: string[], candidateAddresses: string[]) {
-        const [owner, otherAccount, anotherAccountAgain] = await hre.ethers.getSigners();
+        const [setuper, timer, otherAccount] = await hre.ethers.getSigners();
         const Voting = await hre.ethers.getContractFactory("Voting");
-        const voting = await Voting.deploy(candidates, candidateAddresses);
-        return { voting, owner, otherAccount, anotherAccountAgain }
+        const voting = await Voting.deploy(candidates, candidateAddresses, setuper, timer);
+        return { voting, setuper, timer, otherAccount }
     }
-    
-    it("Should deploy the contract and set the right owner", async function () {
-       const {voting, owner} = await deploymentFixture(candidates, candidateAddresses);
-        expect(await voting.owner()).to.equal(owner.address);
-
-    });
 
     it ("Should allow a random person to vote", async function() {
-        const {voting, owner, otherAccount} = await deploymentFixture(candidates, candidateAddresses);
-        await voting.connect(owner).startVotingPeriod();
+        const {voting, timer, otherAccount} = await deploymentFixture(candidates, candidateAddresses);
+        await voting.connect(timer).startVotingPeriod();
         await expect(voting.connect(otherAccount).vote(0, {value: amountToSendWithVote})).not.to.be.reverted;
     })
 
     it ("Should not allow a person to vote twice", async function() {
-        const {voting, owner, otherAccount} = await deploymentFixture(candidates, candidateAddresses);
-        await voting.connect(owner).startVotingPeriod();
+        const {voting, timer, otherAccount} = await deploymentFixture(candidates, candidateAddresses);
+        await voting.connect(timer).startVotingPeriod();
         await voting.connect(otherAccount).vote(0, {value: amountToSendWithVote});
         await expect(voting.connect(otherAccount).vote(0, {value: amountToSendWithVote})).to.be.revertedWith(
             "You already voted!");
@@ -43,85 +37,67 @@ describe("Voting contract", function() {
     })
 
     it ("Should not allow to vote for an unregistered candidate", async function() {
-        const noCandidates: string[] = []
-        const {voting, owner, otherAccount} = await deploymentFixture(noCandidates, candidateAddresses);
-        await voting.connect(owner).startVotingPeriod();
+        const {voting, timer, otherAccount} = await deploymentFixture([], []);
+        await voting.connect(timer).startVotingPeriod();
         await expect(voting.connect(otherAccount).vote(1, {value: amountToSendWithVote})).to.be.revertedWith(
             "The candidate does not exist!");
     })
 
     it ("Should update the number of votes for a candidate", async function() {
-        const {voting, owner, otherAccount} = await deploymentFixture(candidates, candidateAddresses);
-        await voting.connect(owner).startVotingPeriod();
+        const {voting, timer, otherAccount} = await deploymentFixture(candidates, candidateAddresses);
+        await voting.connect(timer).startVotingPeriod();
         await voting.connect(otherAccount).vote(0, {value: amountToSendWithVote});
         const candidate = await voting.getCandidate(0)
         expect(BigInt(candidate.votes)).to.equal(BigInt("1"));
     })
 
     it ("Should not allow votes after the end of the voting period", async function() {
-        const {voting, owner, otherAccount} = await deploymentFixture(candidates, candidateAddresses);
-        await voting.connect(owner).startVotingPeriod();
-        await voting.connect(owner).endVotingPeriod();
+        const {voting, timer, otherAccount} = await deploymentFixture(candidates, candidateAddresses);
+        await voting.connect(timer).startVotingPeriod();
+        await voting.connect(timer).endVotingPeriod();
         await expect(voting.connect(otherAccount).vote(1, {value: amountToSendWithVote})).to.be.revertedWith(
             "Voting period not running!");
     })
 
     it ("Should correctly compute the winner of the election", async function () {
-        const {voting, owner, otherAccount, anotherAccountAgain} = await deploymentFixture(candidates, candidateAddresses);
-        await voting.connect(owner).startVotingPeriod();
-        await voting.connect(owner).vote(0, {value: amountToSendWithVote});
+        const {voting, setuper, timer, otherAccount } = await deploymentFixture(candidates, candidateAddresses);
+        await voting.connect(timer).startVotingPeriod();
+        await voting.connect(timer).vote(0, {value: amountToSendWithVote});
         await voting.connect(otherAccount).vote(1, {value: amountToSendWithVote});
-        await voting.connect(anotherAccountAgain).vote(1, {value: amountToSendWithVote});
-        await voting.connect(owner).endVotingPeriod();
+        await voting.connect(setuper).vote(1, {value: amountToSendWithVote});
+        await voting.connect(timer).endVotingPeriod();
         expect(BigInt((await voting.getWinner())[0])).to.equal(BigInt("1"));
     })
 
-    it ("Should send the nest egg to the winner when ending the voting period", async function() {
-        const {voting, owner} = await deploymentFixture(candidates, candidateAddresses);
-        await voting.connect(owner).startVotingPeriod();
-        await voting.connect(owner).vote(0, {value: amountToSendWithVote});
-
-        const balanceBefore = await hre.ethers.provider.getBalance((await voting.getCandidate(0)).candidate_address);
-        await voting.connect(owner).endVotingPeriod();
-
-        const expectedBalance = balanceBefore + hre.ethers.WeiPerEther
-        const balanceAfter = await hre.ethers.provider.getBalance((await voting.getCandidate(0)).candidate_address);
-
-        expect(balanceAfter).to.equal(expectedBalance);
-    })
-
     it ("Should not compute the winner of the election during the voting period", async function() {
-        const {voting, owner} = await deploymentFixture(candidates, candidateAddresses);
-        await voting.connect(owner).startVotingPeriod();
+        const {voting, timer} = await deploymentFixture(candidates, candidateAddresses);
+        await voting.connect(timer).startVotingPeriod();
         await expect(voting.getWinner()).to.be.revertedWith("The voting period is not finished yet!")
     })
 
-    it ("Should allow anyone to congratulate the winner by sending ETH", async function() {
-        const {voting, owner, otherAccount} = await deploymentFixture(candidates, candidateAddresses);
-        const amountToSend = hre.ethers.parseEther("1.0");
-
-        await voting.connect(owner).startVotingPeriod();
-        await voting.connect(owner).vote(1, {value: amountToSendWithVote});
-        await voting.connect(owner).endVotingPeriod();
-
-        const winner = await voting.getWinner();
-        const balanceBefore = await hre.ethers.provider.getBalance(winner.candidate_address);
-        await voting.connect(otherAccount).congratulateWinner({ value: amountToSend });
-        
-        const expectedBalance = balanceBefore + hre.ethers.WeiPerEther
-        const balanceAfter = await hre.ethers.provider.getBalance(winner.candidate_address);
-
-        expect(balanceAfter).to.equal(expectedBalance);
+    it ("Should allow a voter to withdraw stake", async function() {
+        const {voting, timer } = await deploymentFixture(candidates, candidateAddresses);
+        await voting.connect(timer).startVotingPeriod();
+        await voting.connect(timer).vote(0, {value: amountToSendWithVote});
+        await voting.connect(timer).endVotingPeriod();
+        await expect(voting.connect(timer).withdrawStake()).not.to.be.reverted;
     })
 
-    it ("Should not allow to send 0.0 ETH to the winner", async function() {
-        const {voting, owner, otherAccount} = await deploymentFixture(candidates, candidateAddresses);
-        const amountToSend = hre.ethers.parseEther("0.0");
-
-        await voting.connect(owner).startVotingPeriod();
-        await voting.connect(owner).vote(0, {value: amountToSendWithVote});
-        await voting.connect(owner).endVotingPeriod();
-        await expect(voting.connect(otherAccount).congratulateWinner({ value: amountToSend })).to.be.reverted;
+    it ("Should not allow a voter to withdraw stake twice", async function() {
+        const {voting, timer } = await deploymentFixture(candidates, candidateAddresses);
+        await voting.connect(timer).startVotingPeriod();
+        await voting.connect(timer).vote(0, {value: amountToSendWithVote});
+        await voting.connect(timer).endVotingPeriod();
+        await expect(voting.connect(timer).withdrawStake()).not.to.be.reverted;
+        await expect(voting.connect(timer).withdrawStake()).to.be.reverted;
+    
+    it ("Should allow the setuper to reset the election", async function() {
+        const {voting, timer, setuper } = await deploymentFixture(candidates, candidateAddresses);
+        await voting.connect(timer).startVotingPeriod();
+        await voting.connect(timer).vote(0, {value: amountToSendWithVote});
+        await voting.connect(timer).endVotingPeriod();
+        await expect(voting.connect(setuper).startNewElection(["Joe, Jack"], ["0x98cC8F6627688A48D2A4cc17Cd79741cC67c9DdA", "0x0D476789a3B7C3D19cA3E02394a934bb84fC31D3"])).not.to.be.reverted;
+    })
     })
 });
 
